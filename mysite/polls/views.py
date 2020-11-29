92858d766914b480848f4fb9dd59c1df6b68c97f
aimaimi <yh1127@hotmail.co.jp>
2020-11-29 14:47:34 +0900

    ビューのテスト実装

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -23,6 +23,11 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Question
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 # def results(request, question_id):
 #     question = get_object_or_404(Question, pk=question_id)
